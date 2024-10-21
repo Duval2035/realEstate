dae6b4556b77da4aab90cf768b0b92e17c7cfc0a
ramiell03 <bassoraphaelle@gmail.com>
2024-10-21 12:51:41 +0100

    project

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+  function forget(){
+	alert('data base and security measures not yet available , coming soon')
+  }
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
